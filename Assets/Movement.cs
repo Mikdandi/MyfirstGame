e4a898006a4fc3f3af0f3bebf25d415fb4e38364
Misha <mishakryvoshea@gmail.com>
2020-08-03 11:36:17 +0300

    second commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+
+    public float forwardForse = 2000f;
+    public float sideForse = 500f;
+  
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0,0, forwardForse * Time.deltaTime);
+
+        if( Input.GetKey("d") )
+        {
+            rb.AddForce(sideForse * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForse * Time.deltaTime, 0, 0);
+        }
+    }
+}
